40b19f880da00525b2947dcde002cbf20ceb0585
hnunkung <60070084@kmitl.ac.th>
2019-12-07 14:38:45 +0700

    Update Version 2

--- Assets/Script/TileManager.cs
+++ Assets/Script/TileManager.cs
@@ -10,24 +10,34 @@ public class TileManager : MonoBehaviour
 	private float spawnZ = 0.0f;
 	private float tileLength = 16.0f;
 	private int amnTilesOnScreen = 4;
+    private List<GameObject> activetiles;
+    private float safezone = 16.0f;
+    private int state = 0;
+
     // Start is called before the first frame update
     private void Start()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        activetiles = new List<GameObject>();
+        playerTransform = GameObject.FindWithTag("Player").transform;
         
         for(int i = 0; i < amnTilesOnScreen; i++){
         	SpawnTile();
-        	
         }
     }
 
     // Update is called once per frame
     private void Update()
     {
-    	SpawnTile();
-        // if(playerTransform.position.z > (spawnZ - (amnTilesOnScreen * tileLength))){
-        // 	SpawnTile();
-        // }
+     //   playerTransform = GameObject.FindObjectWithTag("Player").transform;
+     //   print(playerTransform.position);
+    	//SpawnTile();
+        if (playerTransform.position.z - safezone > (spawnZ - (amnTilesOnScreen * tileLength)))
+        {
+            SpawnTile();
+            print("Position z: "+playerTransform.position.z);
+            DeleteTile();
+            print("A tile was Deleted");
+        }
     }
 
     private void SpawnTile(int prefabIndex = -1){
@@ -36,5 +46,13 @@ public class TileManager : MonoBehaviour
     	go.transform.SetParent(transform);
     	go.transform.position = Vector3.forward * spawnZ;
     	spawnZ = spawnZ+tileLength;
+        activetiles.Add(go);
+    }
+
+    void DeleteTile()
+    {
+
+        Destroy(activetiles[0]);
+        activetiles.RemoveAt(0);
     }
 }
--- Assets/Script/movecam.cs
+++ Assets/Script/movecam.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movecam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // GetComponent<Rigidbody>().velocity = new Vector3(0,GM.vertVel,3);
+        disableAnimatorCam();
     }
 
     // Update is called once per frame
     void Update()
     {
-     	GetComponent<Rigidbody>().velocity = new Vector3(0,GM.vertVel,3);   
+        GetComponent<Rigidbody>().velocity = new Vector3(0, GM.vertVel, 3);
     }
+
+    IEnumerable disableAnimatorCam() {
+        yield return new WaitForSeconds(.2f);
+        GetComponent<Animator>().enabled = false;
+    }
+
+    //private void moveCame() {
+
+    //}
 }
