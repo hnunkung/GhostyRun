6a766d343775baefe3970610d6a355b3448a6143
hnunkung <60070084@kmitl.ac.th>
2019-12-08 23:29:26 +0700

    Update Lighting and Coin
    - Fixed Bug coins.
    - Add more Object scene.

--- Assets/Script/effects.cs
+++ Assets/Script/effects.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class effects : MonoBehaviour
 {
+    public GameObject coin;
+    //public bool activeme;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coin.GetComponent<MeshRenderer>().enabled = false;
+        coin.GetComponent<CapsuleCollider>().enabled = false;
+        Debug.Log("Remove");
+        StartCoroutine(setDisactiveObj());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.name == "coin"){
-        	transform.Rotate(0,0,5);
+        if (gameObject.name == "coin")
+        {
+            transform.Rotate(0, 0, 5);
         }
+    }
+
+    IEnumerator setDisactiveObj() {
+        yield return new WaitForSeconds(4.5f);
+        //coin.SetActive(true);
+        coin.GetComponent<MeshRenderer>().enabled = true;
+        coin.GetComponent<CapsuleCollider>().enabled = true;
+        Debug.Log("Back to Active");
     }
 }
